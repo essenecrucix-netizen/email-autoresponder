8a27d1938f7f8cc853c03f8e82d4645cf28f9d3c
essenecrucix-netizen <erikbrisson1@gmail.com>
2024-12-28 13:50:23 -0700

    updated index.js dec 28

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from './App'; // Import the default export from App.js
 
+// ReactDOM renders App into the root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
