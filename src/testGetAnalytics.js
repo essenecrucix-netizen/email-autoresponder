5003b066d3a41deca94d3b283e2bd5bf506ecf70
essenecrucix-netizen <erikbrisson1@gmail.com>
2025-01-05 13:07:41 -0700

    create testGetAnalytics.js

---
+++ src/testGetAnalytics.js
@@ -0,0 +1,10 @@
+const dbService = require('./home/ec2-user/email-autoresponder/src/services/database/DatabaseService')();
+
+(async () => {
+    try {
+        const result = await dbService.getAnalyticsByUser('user123');
+        console.log('Analytics for user123:', result);
+    } catch (error) {
+        console.error('Error retrieving analytics:', error);
+    }
+})();
