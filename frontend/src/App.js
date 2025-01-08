cf2f42cf0bceaa8dacb65a3c082e02cf5403b7b0
essenecrucix-netizen <erikbrisson1@gmail.com>
2025-01-08 15:44:33 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Emails from './components/Emails';
 import Dashboard from './components/Dashboard';
 import KnowledgeBase from './components/KnowledgeBase';
 import Analytics from './components/Analytics';
